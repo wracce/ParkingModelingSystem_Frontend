81436edb507c4556522d8669c0d8305b791f5488
wracce <wracce@gmail.com>
2022-10-13 18:49:32 +0400

    add designet view

--- src/app/view/navbar/navbar.component.ts
+++ src/app/view/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Input() view: string ="";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  setView(view:string): void {
+    this.view = view;
+  }
 }
+
