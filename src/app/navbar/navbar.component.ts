5c7e9f78061e52fb7027c0709d2a00785a4a8ad9
wracce <wracce@gmail.com>
2022-12-29 12:49:32 +0400

    Add fix for login

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,52 +1,52 @@
-import {Component, Injectable, Input, OnInit} from '@angular/core';
+import { Component, Injectable, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ReferenceComponent } from '../reference/reference.component';
-import {UserStorageService} from "../core/service/user-storage.service";
+import { UserStorageService } from '../core/service/user-storage.service';
 import { AuthService } from '../core/service/auth.service';
 import { Router } from '@angular/router';
-
-
+import { DataShareService } from '../core/service/data-share.service';
+import { UserService } from '../core/service/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NavbarComponent implements OnInit {
+  constructor(
+    public dialog: MatDialog,
+    private userStorageService: UserStorageService,
+    public authService: AuthService,
+    private router: Router,
+    private  dataShareService:DataShareService,
+    private userService: UserService
+  ) {}
 
-  constructor(public dialog: MatDialog,
-    private userStorageService: UserStorageService, public authService:AuthService, private router:Router) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   openDialogReference() {
     const dialogRef = this.dialog.open(ReferenceComponent);
   }
 
-  public linkToHome():string {
-    let link="/auth";
-    
-    let role = this.userStorageService.getAuthorities();
-    console.log(role);
+  public routeToHome(): void {
 
-    if(role.localeCompare("ROLE_ADMINISTRATOR"))
-      link = "/administrator";
-    if(role.localeCompare("ROLE_MANAGER"))
-      link = "/manager";
-    return link;
+    let role:string;
+    this.dataShareService.currentUser.subscribe(val=> {
+      this.userService.getByUsername(val).subscribe(val2 => {
+        if (val2.userRole.localeCompare('ROLE_ADMINISTRATOR')) this.router.navigate(['/administrator']);
+        else if (val2.userRole.localeCompare('ROLE_MANAGER')) this.router.navigate(['/manager']);
+        else this.router.navigate(['/manager']);
+      });
+    });
+
   }
 
-  public authInOut():void {
-    if (this.authService.isLoggedIn){
+  public authInOut(): void {
+    if (this.authService.isLoggedIn) {
       this.userStorageService.signOut();
     }
-      // this.router.navigate(['/auth']);
-    }
-
+  }
 }
-
